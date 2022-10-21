d1cb41623ae4cea0cb2c9de27eb4e083d89c5651
mkordic <mirko22kordic@gmail.com>
2022-10-21 20:43:04 +0200

    splash screen duration fixed

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ import 'fragments/notificationPage.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await Future.delayed(const Duration(seconds: 3));
   runApp(const MyApp());
 }
 
